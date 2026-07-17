481f25365a7a052b2628e8166f2336bdc300edc5
dmitriipetrushenko730-hash <dmitriipetrushenko730@gmail.com>
2026-07-17 07:26:30 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,11 +4,8 @@ let currentUser = null;
 let targetUser = null;
 let shareTargetUser = null;
 
-const CLOUD_URL = "https://supabase.co";
-const CLOUD_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
- "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4eHZqZ2ppYnBicn" +
- "NjZGV1dnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NjMy" +
- "MTUsImV4cCI6MjA5NDMzOTIxNX0.7Q_B8R9Z_lX9_vX8_vX7_v" +
- "X6_vX5_vX4_vX3_vX2_vX1_vX0";
+// Запомнил! Твои боевые ключи Supabase:
+const CLOUD_URL = "https://axxvjgjibpbrscdeuvyf.supabase.co";
+const CLOUD_KEY = "sb_publishable_36HNtxEDblwCZC1qCQPd-Q_nbo_c-0s";
 
 let userLangSettings = JSON.parse(localStorage.getItem('w_lang_settings')) || {};
